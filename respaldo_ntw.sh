02cae44c49cac55068997c5b03ef53350af61080
Juan Miranda <jmirandav2@gmail.com>
2017-10-03 17:59:47 -0300

    Update respaldo_ntw.sh

--- respaldo_ntw.sh
+++ respaldo_ntw.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 archivo=bkp_ntw_$(date +%m_%d_%y)
+user-FTP = "usuario"
 passwd-FTP = "clave"
 FTP-Server="FQDN or IP Address"
 mail-rcp = "mail@dominio.corp"
+$asunto-correo="texto del asunto"
 
 cd /home/respaldoNetworking
 
 tar cfjv $archivo.tar.bz2 /tftpboot/
 
-ncftp -u respaldos -p $passwd-FTP $FTP-Server << EOF
+ncftp -u user-FTP -p $passwd-FTP $FTP-Server << EOF
 cd Networking
 put $archivo.tar.bz2
 quit
@@ -17,4 +19,4 @@ EOF
 
 find -name "*.tar.bz2" -mtime +30 -exec rm -f {} \;
 
-echo "Respaldo realizado el $(date)" | mail -s "Respaldos Equipos de Networking" $mail-rcp
+echo "Respaldo realizado el $(date)" | mail -s $asunto-correo $mail-rcp
